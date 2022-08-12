44bed98cde25642a7dc0974c2d920b6f6cd820db
ademola <ademolaowoeye07@gmail.com>
2022-08-12 22:50:44 +0100

    webpack configure

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,17 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader'], 
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+
     ],
   },
 };
