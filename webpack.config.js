9ca801fe226f1cdb11fdb03d17bf8ac47b14f1fb
ademola <ademolaowoeye07@gmail.com>
2022-08-12 22:58:22 +0100

    fixed linters

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'], 
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
