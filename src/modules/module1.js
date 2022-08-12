bf030e0f2380a44e7538e3c07412df5001718c87
ademola <ademolaowoeye07@gmail.com>
2022-08-12 22:53:59 +0100

    module1 js

---
+++ src/modules/module1.js
@@ -0,0 +1,7 @@
+export default class Alltask {
+    constructor(description, completed = false, index) {
+      this.description = description;
+      this.completed = completed;
+      this.index = index;
+    } 
+  }
